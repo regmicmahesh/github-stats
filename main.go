2443477ebcae585838e734335d3d2c2e77c99ed4
Mahesh C. Regmi <0xmahadev@gmail.com>
2021-07-11 23:54:50 +0545

    remove that src bullshit

--- main.go
+++ main.go
@@ -4,16 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 type RequestBody struct {
 	Repository string `json:"repository"`
 }
 
-type ErrorBody struct{
+type ErrorBody struct {
 	ErrorMessage string `json:"error"`
 }
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
@@ -22,27 +22,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&p)
 
-	if err != nil{
+	if err != nil {
 		msg := ErrorBody{
 			ErrorMessage: "Invalid JSON Body",
 		}
 		bytes, _ := json.Marshal(msg)
 		fmt.Fprintln(w, string(bytes))
-		return;
+		return
 	}
 
-	fmt.Println(p)
-
-	//bytes, _ := json.Marshal(p)
 	processRepository(w, p.Repository)
-
 }
 
 func main() {
 
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
 	http.HandleFunc("/", handler)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server running at 0.0.0.0:"+port);
 
+	http.ListenAndServe(":"+port, nil)
 
 }
